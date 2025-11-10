ce1c0bf5f6a02354d11c3578d4ab5ad401e96756
MD. MOHIBUR <md.mohibur@Moheburs-MacBook-Air.local>
2025-11-10 10:34:29 +0600

    get all, get single data, get recent 6 data

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.mgt1ucj.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("ecobills-db");
+    const billsCollection = db.collection("bills");
+
+    //get all
+    app.get("/bills", async (req, res) => {
+      const result = await billsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // get single data
+    app.get("/bills/:id", async (req, res) => {
+      const { id } = req.params;
+      const result = await billsCollection.findOne({ _id: new ObjectId(id) });
+
+      res.send({ success: true, result });
+    });
+
+    // get recent 6 data
+    app.get("/latest-bills", async (req, res) => {
+      const result = await billsCollection
+        .find()
+        .sort({ date: "asc" })
+        .limit(6)
+        .toArray();
+
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
