1c12d502d7bc4a0e2f4b7bef10cc81f5482b37d1
MD. MOHIBUR <md.mohibur@Moheburs-MacBook-Air.local>
2025-11-13 14:58:46 +0600

    varcel deploy complite and submit the repository

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const verifyToken = async (req, res, next) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("ecobills-db");
     const billsCollection = db.collection("bills");
@@ -123,7 +123,7 @@ async function run() {
       });
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
